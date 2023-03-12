8410798e46b48f4d6e8dc6e67662fe3c790c3763
a8vk <a8vk@ya.ru>
2023-03-12 15:46:32 +1000

    added gun, space_defenders

---
+++ space_defenders.py
@@ -0,0 +1,23 @@
+import  pygame  # pip install pygame
+import sys
+from gun import Gun
+
+def run():
+
+    pygame.init()
+    screen = pygame.display.set_mode((700, 800))
+    pygame.display.set_caption("Космические защитники")
+    bg_color = (0, 0, 0)
+    gun = Gun(screen)
+
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        screen.fill(bg_color)
+        gun.output()
+        pygame.display.flip()
+
+run()
